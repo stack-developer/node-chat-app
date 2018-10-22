c02619969913d3be7733216edae11586f1d8f374
Harish Chaudhary <imstackdeveloper@gmail.com>
2018-10-22 23:16:57 +0530

    intial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+const express = require('express');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+// app.get('/',(req,res)=>{
+//     res.send('hello world')
+// });
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+    
+});
+
+
