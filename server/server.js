a89df321fe52839bd1f839555f70707043b96d04
Harish Chaudhary <imstackdeveloper@gmail.com>
2018-10-27 22:09:43 +0530

    Greet new User, and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('New User Connected');
 
+    socket.emit('newMessage',{
+        from:"Admin",
+        text:"Welcome in chat app"
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from:"Admin",
+        text:"New user connected",
+        updateAt: new Date().getTime()
+    })
     
     socket.on('createMessage', (message)=>{
         console.log('createMessage', message);
