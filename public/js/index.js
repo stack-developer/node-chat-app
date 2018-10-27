f5cb53071bd0e0f781a2bbef62df6ead6a6006bb
Harish Chaudhary <imstackdeveloper@gmail.com>
2018-10-27 21:53:56 +0530

    new message is broadcast for every single user

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,6 @@ var socket = io();
         socket.on('connect', ()=>{
             console.log('Connected to Server');
 
-            socket.emit('createMessage',{
-                from:"harish",
-                text:"hey, whats up",
-                createdAt:"12/12/2018"
-            });
-
-        
         });
 
         socket.on('disconnect', ()=>{
